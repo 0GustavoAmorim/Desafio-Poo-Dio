281a603e57e399ade9702bce07cb36c154ae4c5b
Gustavo Amorim <89614046+0GustavoAmorim@users.noreply.github.com>
2022-11-09 12:57:02 -0300

    Add files via upload

---
+++ desafio-poo-celular/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "1197159-6652", modelo: "Iphone 11", imei: "222233", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1197159-5266", modelo: "Nokia tijolão", imei: "332222", memoria: 4);
+iphone.Ligar();
+iphone.InstalarAplicativo("Snake Game");
